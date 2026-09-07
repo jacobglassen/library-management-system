d998885e4f6f856dfcf6210427def95679495fba
Jacob <jacob.lassen@yahoo.dk>
2026-09-07 15:37:50 +0200

    Første version

---
+++ src/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+    final String author;
+    final String title;
+    final String isbn;
+    final int id;
+
+
+    public Book(String author, String title, String isbn, int id) {
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+        this.id = id;
+    }
+    public String toString(){
+        return String.format("""
+                %s: %s; ISBN: %s; %d""",
+                author, title, isbn, id);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+void main() {
+    Book book1 = new Book("George Orwell", "1984", "9780140817744", 1);
+    Book book2 = new Book("Homer", "The Odyssey", "9780060904791", 2);
+    Book book3 = new Book("Kurt Vonnegut", "Slaughterhouse Five", "9780224618014", 3);
+    IO.println(book1);
+    IO.println(book2);
+    IO.println(book3);
+}
